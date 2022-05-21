3e4a02ec3ada4407c35c4f14fd691dc2b5d7ac0a
Brent Gardner <bgardner@squarelabs.net>
2022-05-21 14:12:50 -0600

    Code cleanup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,68 +9,43 @@ import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi
 import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder
 import java.util.*
 
-
-fun main(args: Array<String>) {
-    // Preparing IgniteConfiguration using Java APIs
-    // Preparing IgniteConfiguration using Java APIs
+fun main() {
     val cfg = IgniteConfiguration()
-
-    // The node will be started as a client node.
-
-    // The node will be started as a client node.
-    cfg.setClientMode(true)
-
-    // Classes of custom Java logic will be transferred over the wire from this app.
-
-    // Classes of custom Java logic will be transferred over the wire from this app.
+    cfg.isClientMode = true
     cfg.isPeerClassLoadingEnabled = true
-    cfg.setDeploymentMode(DeploymentMode.CONTINUOUS);
-
-    // Setting up an IP Finder to ensure the client can locate the servers.
-
-    // Setting up an IP Finder to ensure the client can locate the servers.
+    cfg.deploymentMode = DeploymentMode.CONTINUOUS;
     val ipFinder = TcpDiscoveryMulticastIpFinder()
     ipFinder.setAddresses(Collections.singletonList("127.0.0.1:47500..47509"))
     cfg.discoverySpi = TcpDiscoverySpi().setIpFinder(ipFinder)
-
-    // Starting the node
-
-    // Starting the node
     val ignite = Ignition.start(cfg)
-
-    // Create an IgniteCache and put some values in it.
 
     // Create an IgniteCache and put some values in it.
     val cache = ignite.getOrCreateCache<Int, String>("myCache")
     cache.put(1, "Hello")
     cache.put(2, "World!")
-
     println(">> Created the cache and add the values.")
 
     // Executing custom Java compute task on server nodes.
-
-    // Executing custom Java compute task on server nodes.
     ignite.compute(ignite.cluster().forServers()).broadcast(RemoteTask())
-
     println(">> Compute task is executed, check for output on the server nodes.")
-
-    // Disconnect from the cluster.
 
     // Disconnect from the cluster.
     ignite.close()
 }
 
-private class RemoteTask constructor() : IgniteRunnable {
+private class RemoteTask : IgniteRunnable {
     @IgniteInstanceResource
     var ignite: Ignite? = null
     override fun run() {
         println(">> Executing the compute task")
         println(
-            ("   Node ID: " + ignite!!.cluster().localNode().id() + "\n" +
-                    "   OS: " + java.lang.System.getProperty("os.name") +
-                    "   JRE: " + java.lang.System.getProperty("java.runtime.name"))
+            """   
+    Node ID: ${ignite!!.cluster().localNode().id()}
+    OS: ${System.getProperty("os.name")}
+    JRE: ${System.getProperty("java.runtime.name")}
+"""
         )
         val cache: IgniteCache<Int, String> = ignite!!.cache("myCache")
-        println(">> " + cache.get(1) + " " + cache.get(2))
+        println(">> ${cache.get(1)} ${cache.get(2)}")
     }
 }
